144af3d8b05100cf97719c0e64207ba4efc94684
Unknown <rc4350@ship.edu>
2017-06-26 11:25:48 -0400

    gitignore and folders
    added gitignore and set up src and test folders.

---
+++ src/Attack/Attack.java
@@ -0,0 +1,6 @@
+package Attack;
+
+public class Attack
+{
+
+}
---
+++ test/Attack/TestAttack.java
@@ -0,0 +1,18 @@
+package Attack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAttack
+{
+
+	@Test
+	public void testConstruct()
+	{
+		Attack attack = new Attack();
+		assertNotNull(attack);
+				
+	}
+
+}
